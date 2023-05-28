f5393b7faccc4750f14d79bf00d9c96b431debf6
Lucy mac <alan8365@gmail.com>
2023-05-28 13:22:21 +0800

    feat(db): add households and parishioners tables with foreign keys chore(db): update schema version to 2023_05_26_163713
    feat(seeds.rb): add second parishioner and associate with household and spouse instance. Update first parishioner association with household and spouse instance. Update first household association with head of household.
    
    feat(swagger): add endpoints for households CRUD operations
    
    feat(swagger.yaml): add examples for PUT method in user endpoint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_12_181229) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_26_163713) do
+  create_table "households", primary_key: "home_number", id: :string, force: :cascade do |t|
+    t.integer "head_of_household"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "parishioners", force: :cascade do |t|
+    t.string "name"
+    t.string "gender"
+    t.date "birth_at"
+    t.string "postal_code"
+    t.string "address"
+    t.string "photo_url"
+    t.string "father"
+    t.string "mother"
+    t.string "spouse"
+    t.string "home_phone"
+    t.string "mobile_phone"
+    t.string "nationality"
+    t.string "profession"
+    t.string "company_name"
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "spouse_id"
+    t.integer "mother_id"
+    t.integer "father_id"
+    t.string "home_number"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
@@ -22,4 +52,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_12_181229) do
     t.boolean "is_modulator", default: false
   end
 
+  add_foreign_key "households", "parishioners", column: "head_of_household", on_update: :cascade, on_delete: :nullify
+  add_foreign_key "parishioners", "households", column: "home_number", primary_key: "home_number", on_update: :cascade, on_delete: :nullify
+  add_foreign_key "parishioners", "parishioners", column: "father_id", on_update: :cascade, on_delete: :nullify
+  add_foreign_key "parishioners", "parishioners", column: "mother_id", on_update: :cascade, on_delete: :nullify
+  add_foreign_key "parishioners", "parishioners", column: "spouse_id", on_update: :cascade, on_delete: :nullify
 end
