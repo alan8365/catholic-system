2c56d83ed7dbc7f95d72f716ffb7c099149b1601
Lucy mac <alan8365@gmail.com>
2023-06-15 15:05:20 +0800

    feat(db): add fields to parishioner table (sibling_number, children_number, move_in_date, original_parish, move_out_date, move_out_reason, destination_parish)

---
+++ db/migrate/20230615055247_add_fields_to_parishioner.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class AddFieldsToParishioner < ActiveRecord::Migration[7.0]
+  def change
+    add_column :parishioners, :sibling_number, :integer, default: 0
+    add_column :parishioners, :children_number, :integer, default: 0
+
+    add_column :parishioners, :move_in_date, :date, null: true
+    add_column :parishioners, :original_parish, :string, null: true
+
+    add_column :parishioners, :move_out_date, :date, null: true
+    add_column :parishioners, :move_out_reason, :string, null: true
+    add_column :parishioners, :destination_parish, :string, null: true
+  end
+end
