b638b5890ad60846de17198e05a3bdc1edbaba52
Lucy mac <alan8365@gmail.com>
2023-10-03 17:56:31 +0800

    refactor(regular_donations_controller.rb): rename @special_donations to @regular_donations for better clarity and consistency feat(regular_donations_controller.rb): add support for filtering regular donations by a specific date range fix(regular_donations_controller.rb): fix typo in comment for render json, change @special_donations to @regular_donations

--- app/controllers/api/regular_donations_controller.rb
+++ app/controllers/api/regular_donations_controller.rb
@@ -23,19 +23,19 @@ module Api
 
         query_array = string_filed.map { |_| "%#{query}%" }.compact
 
-        @special_donations = RegularDonation.where([query_string, *query_array])
+        @regular_donations = RegularDonation.where([query_string, *query_array])
       elsif date&.match?(%r{\d{4}/\d{1,2}})
         year, month = date.split('/').map(&:to_i)
 
         begin_date = Date.civil(year, month, 1)
         end_date = Date.civil(year, month, -1)
 
-        @special_donations = RegularDonation.where(donation_at: begin_date..end_date)
+        @regular_donations = RegularDonation.where(donation_at: begin_date..end_date)
       else
-        @special_donations = RegularDonation.all
+        @regular_donations = RegularDonation.all
       end
 
-      @special_donations = @special_donations
+      @regular_donations = @regular_donations
                            .select(*%w[
                                      id
                                      home_number
@@ -44,7 +44,7 @@ module Api
                                      comment
                                    ])
 
-      render json: @special_donations, include: { household: { include: :head_of_household } }, status: :ok
+      render json: @regular_donations, include: { household: { include: :head_of_household } }, status: :ok
     end
 
     # GET /regular_donations/{id}
